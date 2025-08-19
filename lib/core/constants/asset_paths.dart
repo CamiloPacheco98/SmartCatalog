96c30c203e484cf755a7b3bdef6a41f93c2c2dea
Camilo Pacheco <camilopacheco2@outlook.com>
2025-08-19 16:47:25 -0500

    Add cart localization and UI enhancements: Introduce English and Spanish translations for cart messages, add an empty cart view with an image and button, and update the CartView to utilize localized strings for improved user experience.

--- lib/core/constants/asset_paths.dart
+++ lib/core/constants/asset_paths.dart
@@ -1,6 +1,7 @@
 class AssetPaths {
   // images
   static const String logo = 'assets/images/logo.png';
+  static const String emptyBag = 'assets/images/empty_bag.png';
 
   // animations
   static const String shoppingLoader = 'assets/animations/shopping_loader.json';
--- lib/features/cart/presentation/page/cart_view.dart
+++ lib/features/cart/presentation/page/cart_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:smart_catalog/core/constants/asset_paths.dart';
 import 'package:smart_catalog/features/cart/presentation/models/cart_product_view_model.dart';
 import 'package:go_router/go_router.dart';
+import 'package:smart_catalog/extensions/context_extensions.dart';
 
 class CartView extends StatelessWidget {
   final List<CartProductViewModel> products;
@@ -11,14 +14,37 @@ class CartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cart Page'),
+        title: Text('cart.title'.tr(), style: context.textTheme.labelLarge),
         leading: IconButton(
           onPressed: () => context.pop(),
           icon: const Icon(Icons.arrow_back),
         ),
       ),
       body: products.isEmpty
-          ? const Center(child: Text('No products in cart'))
+          ? Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(AssetPaths.emptyBag, width: 120, height: 120),
+                  Text(
+                    'cart.no_products_title'.tr(),
+                    style: context.textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'cart.no_products_subtitle'.tr(),
+                    style: context.textTheme.bodyLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => context.pop(),
+                    child: Text('cart.no_products_button'.tr()),
+                  ),
+                ],
+              ),
+            )
           : ListView.builder(
               itemCount: products.length,
               itemBuilder: (context, index) => Text(products[index].id),
