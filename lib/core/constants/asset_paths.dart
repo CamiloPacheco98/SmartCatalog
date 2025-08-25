5133335e6f49fd614b5a1bca8f3979c28c9b50a7
Camilo Pacheco <camilopacheco2@outlook.com>
2025-08-25 16:29:16 -0500

    Enhance OrdersView with empty state UI: Add no orders title and subtitle translations in English and Spanish, and include an empty orders image in the asset paths.

--- lib/core/constants/asset_paths.dart
+++ lib/core/constants/asset_paths.dart
@@ -2,6 +2,7 @@ class AssetPaths {
   // images
   static const String logo = 'assets/images/logo.png';
   static const String emptyBag = 'assets/images/empty_bag.png';
+  static const String emptyOrders = 'assets/images/empty_orders.png';
 
   // animations
   static const String shoppingLoader = 'assets/animations/shopping_loader.json';
--- lib/features/orders/presentation/page/orders_view.dart
+++ lib/features/orders/presentation/page/orders_view.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:smart_catalog/extensions/context_extensions.dart';
+import 'package:smart_catalog/core/constants/asset_paths.dart';
 
 class OrdersView extends StatelessWidget {
   const OrdersView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('orders.title'.tr())));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('orders.title'.tr(), style: context.textTheme.labelLarge),
+      ),
+      body: _buildEmptyCart(context),
+    );
+  }
+
+  Widget _buildEmptyCart(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(AssetPaths.emptyOrders, width: 150, height: 150),
+          Text(
+            'orders.no_orders_title'.tr(),
+            style: context.textTheme.titleLarge,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'orders.no_orders_subtitle'.tr(),
+            style: context.textTheme.bodyLarge,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
   }
 }
