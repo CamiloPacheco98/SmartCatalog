883b33e520422a94ccee4646698dade138730a73
Camilo Pacheco <camilopacheco2@outlook.com>
2025-08-27 09:49:28 -0500

    Fix shared reference issue in OrdersCubit: Update _orders initialization to use a copy of the provided orders list for proper BLoC pattern adherence.

--- lib/features/orders/presentation/cubit/orders_cubit.dart
+++ lib/features/orders/presentation/cubit/orders_cubit.dart
@@ -7,6 +7,9 @@ part 'orders_state.dart';
 class OrdersCubit extends Cubit<OrdersState> {
   late final List<OrderEntity> _orders;
   OrdersCubit({required List<OrderEntity> orders}) : super(OrdersInitial()) {
+    // TODO: Fix accidental shared reference - OrdersCubit should use immutable state
+    // Currently works because _orders shares reference with OrdersSession._orders
+    // Should create copy: _orders = List.from(orders) for proper BLoC pattern
     _orders = orders;
     emit(OrdersLoaded(_orders));
   }
